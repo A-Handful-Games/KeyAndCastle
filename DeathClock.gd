ae25710920baac9b387fe7fd05f159f9766cbd37
Tahv Demayo <tahvsprojects@gmail.com>
2023-05-31 13:05:55 -0700

    Added script functionality

--- DeathClock.gd
+++ DeathClock.gd
@@ -1,9 +1,11 @@
 extends Timer
 
-#this was left blank
+func suck_my_nut(dir):
+	print_debug("Suck my ", dir, " nut.")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	suck_my_nut("left")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
