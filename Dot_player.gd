9a18c8606c792e6082667a50ab0e0472b2012980
wetda3 <124001666+sdemayo@users.noreply.github.com>
2023-06-04 16:14:36 -0700

    Added grid and made movement code worse

--- Dot_player.gd
+++ Dot_player.gd
@@ -1,30 +1,30 @@
 extends CharacterBody2D
 
-@export var move_speed : float = 4
+@export var move_speed : float = 16
 
 
 
 	
 func _physics_process(delta):
 	
+	#var test = Input.get_vector("left","right","up","down")
+	var input_direction = Vector2(0,0)
+	if Input.is_action_just_pressed("up"):
+		input_direction = Vector2(0,-1)
+	if Input.is_action_just_pressed("down"):
+		input_direction = Vector2(0,1)
+	if Input.is_action_just_pressed("right"):
+		input_direction = Vector2(1,0)
+	if Input.is_action_just_pressed("left"):
+		input_direction = Vector2(-1,0)
+
+	velocity = input_direction * move_speed
+	position = position + velocity
 	
-	
-	
-	var has_action_been_pressed = Input.is_action_just_pressed("up") or Input.is_action_just_pressed("down") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right")
-	
-	if has_action_been_pressed:
-		var x = Input.get_action_strength("right") - Input.get_action_strength("left")
-		var y = Input.get_action_strength("down") - Input.get_action_strength("up")
-		var input_direction = Vector2(x,y)
-		
+	if input_direction != Vector2(0,0):
 		print(input_direction)
-	
-		velocity  = input_direction * move_speed
-		position = position + velocity
 		print(position)
 	
-
-
 
 
 
