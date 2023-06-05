3446f26b77f048f094c752f9086f884ce038554d
wetda3 <124001666+sdemayo@users.noreply.github.com>
2023-06-04 21:53:44 -0700

    Trying to get key collision

--- Scripts/Dot_player.gd
+++ Scripts/Dot_player.gd
@@ -9,13 +9,9 @@ func _physics_process(delta):
 	
 	var input_direction = Vector2(0,0)
 	
-	#only run the other if statements if something is pressed
-	if Input.is_anything_pressed() == false:
-		pass
-	
 	# -1 is up and +1 is down (opposite of what you think)
 	# -1 is left and +1 is right
-	elif Input.is_action_just_pressed("up"):
+	if Input.is_action_just_pressed("up"):
 		input_direction = Vector2(0,-1)
 	elif Input.is_action_just_pressed("down"):
 		input_direction = Vector2(0,1)
