90f43053511ebcbbf5c82e78e1fdfc7e97386c84
wetda3 <124001666+sdemayo@users.noreply.github.com>
2023-06-05 12:17:57 -0700

    Now handles movement in _input

--- Scripts/Dot_player.gd
+++ Scripts/Dot_player.gd
@@ -3,32 +3,26 @@ extends CharacterBody2D
 @export var move_speed : float = 16
 
 
-
-	
-func _physics_process(delta):
-	
-	var input_direction = Vector2(0,0)
-	
-	# -1 is up and +1 is down (opposite of what you think)
-	# -1 is left and +1 is right
-	if Input.is_action_just_pressed("up"):
-		input_direction = Vector2(0,-1)
-	elif Input.is_action_just_pressed("down"):
-		input_direction = Vector2(0,1)
-	elif Input.is_action_just_pressed("right"):
-		input_direction = Vector2(1,0)
-	elif Input.is_action_just_pressed("left"):
-		input_direction = Vector2(-1,0)
-
-	velocity = input_direction * move_speed
-	position = position + velocity
-	
-	if input_direction != Vector2(0,0):
-		print(input_direction)
-		print(position)
-	
-
-
-
-
+func _input(event):
+	if event is InputEventKey:
+		
+		var input_direction = Vector2(0,0)
+		
+		var x = char(event.unicode)
+		match x:
+			"w":
+				input_direction = Vector2(0,-1)
+			"a":
+				input_direction = Vector2(-1,0)
+			"s":
+				input_direction = Vector2(0,1)
+			"d":
+				input_direction = Vector2(1,0)
+				
+		velocity = input_direction * move_speed
+		position = position + velocity
+				
+		if input_direction != Vector2(0,0):
+			print(input_direction)
+			print(position)
 
