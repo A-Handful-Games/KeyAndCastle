9ecce99267300b68f35c83aaba7b20a00e1d83fa
wetda3 <124001666+sdemayo@users.noreply.github.com>
2023-06-09 16:40:24 -0700

    +1

--- Camera2D.gd
+++ Camera2D.gd
@@ -21,8 +21,8 @@ func _on_our_tile_grid_proportions(tile_scale, grid_size, tile_size):
 	
 	grid_size = Vector2(grid_size*tile_size)
 	grid_center = grid_size/2
-	screen_size = get_viewport_rect().size #(1920, 1009) full screen
-	ratio = screen_size/(grid_size)
+	screen_size = get_viewport_rect().size
+	ratio = screen_size/grid_size
 	
 	#Centers camera at grid center
 	position = grid_center
@@ -32,19 +32,20 @@ func _on_our_tile_grid_proportions(tile_scale, grid_size, tile_size):
 		zoom = ratio
 	else:
 		
-		#take diffenence between grid size and screen size
-		var diff : Vector2 = screen_size-grid_size
 		var index_diff : int
 		
 		if grid_size > screen_size:
-			print("grid size bigger - Chose max_diff - make grid smaller")
+			#make grid smaller
 			
 			#if grid is bigger than screen size take the index of the axis that is bigger
 			index_diff = grid_size.max_axis_index()
 			
 			
 		elif grid_size < screen_size:
-			print("Screen size bigger - Chose min_diff - make grid bigger")
+			#make grid bigger
+			
+			#take diffenence between grid size and screen size
+			var diff = Vector2(screen_size-grid_size)
 			
 			#if grid is samller than screen size get the axis in which the 
 			#difference between the grid and the sccreen is the smallest
@@ -53,13 +54,7 @@ func _on_our_tile_grid_proportions(tile_scale, grid_size, tile_size):
 		
 
 		#size of the camera/what size the camera should be
-		var scale_by = (grid_size[index_diff] + diff[index_diff])/grid_size[index_diff]
-		print(screen_size/grid_size)
-		
-		print("Scale: ",scale_by)
-		print("grid_size: ",grid_size)
-		print("index: ",index_diff)
-		print("diff: ",diff)
+		var scale_by = ratio[index_diff]
 		
 		zoom = Vector2(scale_by,scale_by)
 		pass
