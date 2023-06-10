1762fe219b8873c1e5178fe823d9eacf6796e542
wetda3 <124001666+sdemayo@users.noreply.github.com>
2023-06-09 18:15:12 -0700

    +2

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,8 +1,5 @@
 extends Camera2D
-var grid_size : Vector2
-var grid_center : Vector2
-var screen_size : Vector2
-var ratio : Vector2
+
 
 @export var Take_up_full_screen : bool = false
 
@@ -17,45 +14,50 @@ func _process(delta):
 	pass
 
 
-func _on_our_tile_grid_proportions(tile_scale, grid_size, tile_size):
+func _on_our_tile_grid_proportions(tile_scale, grid_size_in_tiles, tile_size):
 	
-	grid_size = Vector2(grid_size*tile_size)
-	grid_center = grid_size/2
-	screen_size = get_viewport_rect().size
-	ratio = screen_size/grid_size
+	var grid_size = Vector2(grid_size_in_tiles*tile_size)
+	var grid_center = grid_size/2
+	var screen_size = get_viewport_rect().size
+	var ratio = screen_size/grid_size
+
 	
 	#Centers camera at grid center
 	position = grid_center
 	
+	zoom = calc_camera_zoom(
+		grid_size,
+		grid_center,
+		screen_size,
+		ratio
+	)
+
+
+func calc_camera_zoom(grid_size,grid_center,screen_size,ratio)->Vector2:
+
+	
 	if Take_up_full_screen == true:
 		#if true makes grid take up full screen
-		zoom = ratio
-	else:
+		return ratio
 		
-		var index_diff : int
+	
+	#make grid smaller
+	var index_diff : int = grid_size.max_axis_index()
+	
+	if grid_size < screen_size:
+		#make grid bigger
 		
-		if grid_size > screen_size:
-			#make grid smaller
-			
-			#if grid is bigger than screen size take the index of the axis that is bigger
-			index_diff = grid_size.max_axis_index()
-			
-			
-		elif grid_size < screen_size:
-			#make grid bigger
-			
-			#take diffenence between grid size and screen size
-			var diff = Vector2(screen_size-grid_size)
-			
-			#if grid is samller than screen size get the axis in which the 
-			#difference between the grid and the sccreen is the smallest
-			index_diff = diff.min_axis_index()
+		#take diffenence between grid size and screen size
+		var diff = Vector2(screen_size-grid_size)
+		
+		#if grid is samller than screen size get the axis in which the 
+		#difference between the grid and the sccreen is the smallest
+		index_diff = diff.min_axis_index()
 			
 		
+	#size of the camera/what size the camera should be
+	var scale_by = ratio[index_diff]
 
-		#size of the camera/what size the camera should be
-		var scale_by = ratio[index_diff]
-		
-		zoom = Vector2(scale_by,scale_by)
-		pass
+	return Vector2(scale_by,scale_by)
+
 
