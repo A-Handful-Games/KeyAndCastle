66fbed9eee5cfd2f85bf2dd7f0a2b33077b00414
Tahv Demayo <tahvsprojects@gmail.com>
2023-06-12 15:41:18 -0700

    Initial Commit

--- Scripts/Local ECS/Entities/PlayerEntity.gd
+++ Scripts/Local ECS/Entities/PlayerEntity.gd
@@ -1,12 +1,15 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var debug : bool = false
+class_name PlayerEntity
+
+func _init():
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_tree_pretty()
-	print(PositionComponent.new(Vector2.ONE).data)
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass # Replace with function body.
 	pass
