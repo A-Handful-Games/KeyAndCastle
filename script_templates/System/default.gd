b91b499af24862c1980a6caebc2f2e22f479964f
Tahv Demayo <tahvsprojects@gmail.com>
2023-06-16 17:45:34 -0700

    Default Template for Systems

---
+++ script_templates/System/default.gd
@@ -0,0 +1,18 @@
+extends System
+class_name PlayerMovementSystem
+
+
+# here system logic
+func on_entity_process(entity: Entity, delta: float):
+	pass
+
+
+# returns an array of necessary components
+func get_necessary_components():
+	pass
+
+
+# returns an array of banned components
+func get_banned_components():
+	pass
+
